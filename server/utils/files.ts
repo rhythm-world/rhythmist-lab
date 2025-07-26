dcdc9933d742695572d1ca62adc786a8430d35c6
Typed SIGTERM <typed.sigterm@gmail.com>
2025-07-27 00:10:09 +0800

    fix

--- server/utils/files.ts
+++ server/utils/files.ts
@@ -3,11 +3,9 @@ import { Buffer } from 'node:buffer';
 export function toBase64(x: undefined | null | ArrayBuffer | Buffer | Uint8Array) {
   if (x == null)
     return '';
-  if (x instanceof ArrayBuffer)
-    return Buffer.from(x).toString('base64');
-  if (x instanceof Buffer)
+  if (Buffer.isBuffer(x))
     return x.toString('base64');
-  if (x instanceof Uint8Array)
+  if (x instanceof ArrayBuffer || x instanceof Uint8Array)
     return Buffer.from(x).toString('base64');
   throw new Error(`Unknown type: ${Object.getPrototypeOf(x)?.constructor?.name}`);
 }
